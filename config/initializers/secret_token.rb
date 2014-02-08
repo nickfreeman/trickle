c4f520c925b937c3ca7d10a23945987e0f25608d
Nick Freeman <nickafreeman@berkeley.edu>
2014-02-08 15:14:36 -0800

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Trickle::Application.config.secret_key_base = '850ed05ab59af7bd9123dba2592f4aa00a24089bdee143f8c7b6785a77fda6108923e4ac18f8f3c293bdb0094c4106f86f35c2ed8f8615371cd43b72efa3d4f3'
