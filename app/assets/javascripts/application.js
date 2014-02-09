7fd26787d5c3df99b7bffae3ea8bcecd5fa79255
Nick Freeman <nickafreeman@berkeley.edu>
2014-02-08 18:16:35 -0800

    fixed foundation

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,8 +13,6 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
-//= require foundation
 //= require_tree .
-$(function() {
-  $(document).foundation();
-});
+//= require foundation
+$(document).foundation();
